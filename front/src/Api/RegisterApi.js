b87a2deda9323c7fcf884ff8b759be148ec833c9
ElderGr <gbielrs@gmail.com>
2023-02-16 12:56:11 -0300

    fix: code review

--- front/src/Api/RegisterApi.js
+++
@@ -1,6 +0,0 @@
-import api from "./Api";
-
-export async function registerNewUserApi(body, config) {
-    const response = await api.post('/registration/newuser', body, config);
-    return response.data;
-}
